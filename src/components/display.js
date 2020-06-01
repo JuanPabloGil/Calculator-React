1572a622827e7baa0c03f946e888e50aba476db1
Juan Pablo <jpablomgil@gmail.com>
2020-06-01 14:51:47 -0500

    Create prop of the string to outputs the result

---
+++ src/components/display.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Display = ({ result }) => (
+  <div className="Display">
+    <h1>{result}</h1>
+  </div>
+);
+
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
+};
+
+export default Display;
