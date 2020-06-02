ac19acf97e17c07db86b56d73b62756809f239d0
Juan Pablo <jpablomgil@gmail.com>
2020-06-02 11:08:26 -0500

    Create styleClass property to pass wide and color

--- src/components/button.js
+++ src/components/button.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../style/button.css';
 
 
-const Button = ({ name }) => (
+const Button = ({ name, styleClass }) => (
 
   <div className="component-display">
-    <button type="button">{name}</button>
+    <button className={styleClass} type="button">{name}</button>
   </div>
 );
 
 
 Button.propTypes = {
   name: PropTypes.string,
+  styleClass: PropTypes.string,
 };
 Button.defaultProps = {
   name: 'n/a',
+  styleClass: '',
 };
 
 export default Button;
