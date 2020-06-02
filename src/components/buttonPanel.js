8a283b6bd3e73083c0c01adda5e13cb519392f8b
Juan Pablo <jpablomgil@gmail.com>
2020-06-02 11:10:52 -0500

    Pass prop and center content

--- src/components/buttonPanel.js
+++ src/components/buttonPanel.js
@@ -10,32 +10,32 @@ const ButtonPanel = () => (
       <Button name="AC" />
       <Button name="+/-" />
       <Button name="%" />
-      <Button name="÷" />
+      <Button name="÷" styleClass="button-orange" />
     </div>
     <div>
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
-      <Button name="X" />
+      <Button name="X" styleClass="button-orange" />
 
     </div>
     <div>
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
-      <Button name="-" />
+      <Button name="-" styleClass="button-orange" />
 
     </div>
     <div>
       <Button name="1" />
       <Button name="2" />
       <Button name="3" />
-      <Button name="+" />
+      <Button name="+" styleClass="button-orange" />
     </div>
     <div>
-      <Button name="0" />
+      <Button name="0" styleClass="button-big" />
       <Button name="." />
-      <Button name="=" />
+      <Button name="=" styleClass="button-orange" />
     </div>
   </div>
 );
