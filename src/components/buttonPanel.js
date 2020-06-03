030c5de722bef2f95f390803c980928db761a3f3
Juan Pablo <jpablomgil@gmail.com>
2020-06-03 09:43:28 -0500

    Pass property wide and orange to the button component

--- src/components/buttonPanel.js
+++ src/components/buttonPanel.js
@@ -3,41 +3,41 @@ import Button from './button';
 import '../style/buttonPanel.css';
 
 
-const ButtonPanel = () => (
+export default class ButtonPanel extends React.Component {
 
-  <div className="ButtonPanel">
-    <div>
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" styleClass="button-orange" />
-    </div>
-    <div>
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="X" styleClass="button-orange" />
-
-    </div>
-    <div>
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
-      <Button name="-" styleClass="button-orange" />
-
-    </div>
-    <div>
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
-      <Button name="+" styleClass="button-orange" />
-    </div>
-    <div>
-      <Button name="0" styleClass="button-big" />
-      <Button name="." />
-      <Button name="=" styleClass="button-orange" />
-    </div>
-  </div>
-);
-
-export default ButtonPanel;
+  render() {
+    return (
+      <div className="component-button-panel">
+        <div>
+          <Button name="AC" />
+          <Button name="+/-" />
+          <Button name="%" />
+          <Button name="÷" orange />
+        </div>
+        <div>
+          <Button name="7" />
+          <Button name="8" />
+          <Button name="9" />
+          <Button name="x" orange />
+        </div>
+        <div>
+          <Button name="4" />
+          <Button name="5" />
+          <Button name="6" />
+          <Button name="-" orange />
+        </div>
+        <div>
+          <Button name="1" />
+          <Button name="2" />
+          <Button name="3" />
+          <Button name="+" orange />
+        </div>
+        <div>
+          <Button name="0" wide />
+          <Button name="." />
+          <Button name="=" orange />
+        </div>
+      </div>
+    );
+  }
+}
