e853602478ac0ab1ea9e4cfffa86fac28ccba43e
Juan Pablo <jpablomgil@gmail.com>
2020-06-05 16:46:47 -0500

    refactor button to pass handler

--- src/components/button.js
+++ src/components/button.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import '../style/button.css';
 
-class Button extends React.Component {
-  constructor(prop) {
-    super(prop);
-    Button.propTypes = {
-      name: PropTypes.string,
-      orange: PropTypes.bool,
-      wide: PropTypes.bool,
-    };
-    Button.defaultProps = {
-      name: 'n/a',
-      orange: true,
-      wide: false,
-    };
-  }
+class Button extends React.Component {  
 
+  handleClick = () => {
+    this.props.clickHandler(this.props.name);
+  }
+ 
   render() {
-    const properties = this.props;
+ 
     const className = [
-      'component-button',
-      properties.orange ? 'orange' : '',
-      properties.wide ? 'wide' : '',
+      "component-button",
+      this.props.orange ? "orange" : "",
+      this.props.wide ? "wide" : "",
     ];
 
     return (
-      <div className={className.join(' ').trim()}>
-        <button type="button">{properties.name}</button>
+      <div className={className.join(" ").trim()}>
+        <button onClick={this.handleClick}>{this.props.name}</button>
       </div>
     );
   }
 }
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  orange: PropTypes.bool,
+  wide: PropTypes.bool,
+};
+
 
 export default Button;
