5b24e3ff5a19657c389f0729c819dc39cbf287cb
Juan Pablo <jpablomgil@gmail.com>
2020-06-05 16:46:09 -0500

    Refactor App.js to pass handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Display from './display';
 import ButtonPanel from './buttonPanel';
+import calculate from '../logic/calculate';
 import '../style/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      next: null,
+      total: null,
+      operation: null
+    }
+  }
+
+  clickHandler = (buttonName) => {
+    this.setState(calculate(this.state, buttonName));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Display result={this.state.total || '0'} />
+        <ButtonPanel clickHandler={this.clickHandler}/>
+      </div>
+    )
+  }
 }
 
 export default App;
